6f8a0b3e0af5df9baf130ae09ab16236a80eba9d
Shubham Kumar <hello@shubhkumar.in>
2020-12-06 14:41:13 +0530

    remove url log.

--- utils/htb.js
+++ utils/htb.js
@@ -5,7 +5,6 @@ const token = process.env.HTB_TOKEN
 
 const getBoxes = async ()=>{
   let url = `${base}/machines/get/all?api_token=${token}`
-  console.log(url);
   let response = await axios.get(url)
   return response.data.filter(x => x.retired == false)
 
