0a18694fa70c0554f46c74e1c8ab3ea75c36a920
Shubham Kumar <hello@shubhkumar.in>
2020-12-07 10:38:40 +0530

    Added Starting Point Auto Create.

--- utils/htb.js
+++ utils/htb.js
@@ -3,12 +3,25 @@ const base = "https://www.hackthebox.eu/api"
 const token = process.env.HTB_TOKEN
 
 
-const getBoxes = async ()=>{
-  let url = `${base}/machines/get/all?api_token=${token}`
-  let response = await axios.get(url)
-  return response.data.filter(x => x.retired == false)
+const getBoxes = async () => {
+    let url = `${base}/machines/get/all?api_token=${token}`
+    let response = await axios.get(url)
+    return response.data.filter(x => x.retired == false)
+
+}
+//
+// curl --location --request GET 'https://www.hackthebox.eu/api/startingpoint/machines' \
+// --header 'Authorization: Bearer rSCd2lsr1pP79waOsGMcIofa9AFWg0BEgbJYuJmYvsB2jLx7ybV9yDrWkyEo'
+const getStartingPointBox = async () => {
+    let url = `${base}/startingpoint/machines`
+    let headers = {
+        "Authorization": `Bearer ${token}`
+    }
+    let response = await axios.get(url,{headers})
+    return response.data
 
 }
 
 
 exports.getBoxes = getBoxes;
+exports.getStartingPointBox = getStartingPointBox;
