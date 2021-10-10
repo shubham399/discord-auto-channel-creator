c64316945399336d4cd5cf532cb16286b3d5c947
Shubham Kumar <hello@shubhkumar.in>
2021-10-10 10:04:31 +0530

    Update htb.js

--- utils/htb.js
+++ utils/htb.js
@@ -4,20 +4,30 @@ const token = process.env.HTB_TOKEN
 
 
 const getBoxes = async () => {
-    let url = `${base}/machines/get/all?api_token=${token}`
-    let response = await axios.get(url)
-    return response.data.filter(x => x.retired == false)
+    try {
+        let url = `${base}/machines/get/all?api_token=${token}`
+        let response = await axios.get(url)
+        return response.data.filter(x => x.retired == false)
+    }
+    catch (e) {
+        console.error("getBoxes Error", e.message)
+    }
 
 }
 //
 // curl --location --request GET 'https://www.hackthebox.eu/api/startingpoint/machines' \
 const getStartingPointBox = async () => {
-    let url = `${base}/startingpoint/machines`
-    let headers = {
-        "Authorization": `Bearer ${token}`
+    try {
+        let url = `${base}/startingpoint/machines`
+        let headers = {
+            "Authorization": `Bearer ${token}`
+        }
+        let response = await axios.get(url, { headers })
+        return response.data
     }
-    let response = await axios.get(url,{headers})
-    return response.data
+    catch (e) {
+        console.error("getStartingPointBox Error", e.message)
+    }
 
 }
 
